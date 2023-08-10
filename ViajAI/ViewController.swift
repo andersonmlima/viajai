00a159d537b1206a74b32e85ac3dac96f5aa7490
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-08-10 19:23:32 -0300

    Initial Commit

---
+++ ViajAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViajAI
+//
+//  Created by Reinaldo Neto on 10/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
