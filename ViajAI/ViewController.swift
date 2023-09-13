3eb66c98af423efa368ae3fedaff6e66164b5ca2
Felipe Miranda Santos <felipe_miranda92@outlook.com>
2023-09-12 23:20:08 -0300

    SplashScreen

--- ViajAI/ViewController.swift
+++ ViajAI/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoLabel: UILabel!
+    
+    @IBOutlet weak var firstMessageLabel: UILabel!
+    
+    @IBOutlet weak var secondMessageLabel: UILabel!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configElements()
     }
 
-
+    
+    func configElements() {
+        logoLabel.text = "ViajAÍ"
+        logoLabel.textAlignment = .center
+        
+        firstMessageLabel.text = "Comece sua viagem com segurança e diversão"
+        firstMessageLabel.textAlignment = .center
+        firstMessageLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        firstMessageLabel.textColor = UIColor.black
+        firstMessageLabel.numberOfLines = 0
+        firstMessageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        secondMessageLabel.text = "A ideia do ViajAÍ é analisar as preferências do usuário, incluindo interesses em atrações turísticas, atividades culturais, gastronomia, orçamento, entre outros, para criar roteiros personalizados para cada viagem.                      Viaje sem medo!"
+        secondMessageLabel.textAlignment = .center
+        secondMessageLabel.font = UIFont.systemFont(ofSize: 15)
+        secondMessageLabel.textColor = UIColor.gray
+        secondMessageLabel.numberOfLines = 0
+        secondMessageLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
 }
 
