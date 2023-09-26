a3177f77be47ec56191f4d6197bf93b7254ab2d4
Anderson Moura <>
2023-09-26 10:08:53 -0400

    Teste tela Favorites

---
+++ ViajAI/Modules/Favorites/FavoritesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesViewController.swift
+//  ViajAI
+//
+//  Created by Anderson Moura on 26/09/23.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
