910a1bc7a67b736d02b7976c41fb2b2edcbc43d6
Fábio Oliveira Alegre <fabioalegre12@gmail.com>
2023-09-27 18:29:33 -0300

    Arrumada toda storyboard adicionada funcoes de escrita para cada texfield, e funcoes para os botoes

--- ViajAI/Register/RegisterViewController.swift
+++ ViajAI/Register/RegisterViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var logoImageView: UIImageView!
     
@@ -28,9 +28,63 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        nameTextField.keyboardType = .default
+        emailTextField.keyboardType = .emailAddress
+        passwordTextField.isSecureTextEntry = true
+        
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        registerButton.isEnabled = false
+        //registerButton.setTitle("Cadastrar", (0.4), for: .disabled)
+        //registerButton.setTitle("Cadastrar", for: .normal)
     }
+    
+    @IBAction func tappedRegisterButton(_ sender:
+                                        UIButton) {
+    }
+    
+    func validateTextField() {
+        if nameTextField.text != "" && emailTextField.text != "" && passwordTextField.text != "" {
+            print(nameTextField.text ?? "")
+            print(emailTextField.text ?? "")
+            print(passwordTextField.text ?? "")
+            
+            registerButton.isEnabled = true
+            
+        } else {
+            registerButton.isEnabled = false
+            
+        }
+        
+        
+    }
+    
+}
+    
+    extension ViewController: UITextFieldDelegate {
+        
+        func textFieldDidBeginEditing(_ textField: UITextField) {
+            print(#function)
+            textField.layer.borderColor = UIColor.blue.cgColor
+            textField.layer.borderWidth = 1
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            print(#function)
+            textField.layer.borderWidth = 0
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            print(#function)
+            textField.resignFirstResponder()
+            return true
+        }
+        
+    }
+    
     
 
   
-}
+
