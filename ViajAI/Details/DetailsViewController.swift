c830c3b5026faf501f6ddfefea32200effca456f
Michael Bressiani <>
2023-09-30 12:27:12 -0300

    Tela de detalhes v1

--- ViajAI/Details/DetailsViewController.swift
+++ ViajAI/Details/DetailsViewController.swift
@@ -9,15 +9,28 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var titleLocalLabel: UILabel!
+    @IBOutlet weak var descriptionLocalLabel: UILabel!
     @IBOutlet weak var imageLocalImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageLocalImageView.image = UIImage(named: "rio")
-        imageLocalImageView.contentMode = .scaleAspectFill
+        elementsConfig()
     }
     
     func elementsConfig() {
+        imageLocalImageView.image = UIImage(named: "rio")
+        imageLocalImageView.contentMode = .scaleAspectFill
+        titleLocalLabel.font = UIFont.systemFont(ofSize: 20)
+        descriptionLocalLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        
+        titleLocalLabel.textColor = UIColor.black
+        descriptionLocalLabel.textColor = UIColor.gray
+        
+        descriptionLocalLabel.numberOfLines = 20
+        
+        titleLocalLabel.text = "Praia de Copacabana"
+        descriptionLocalLabel.text = "Copacabana é um bairro situado na Zona Sul do município do Rio de Janeiro, no Brasil. É considerado um dos bairros mais famosos e prestigiados do Brasil e um dos mais conhecidos do mundo. Tem o apelido de Princesinha do Mar e Coração da Zona Sul. Faz limites com os bairros da Lagoa, Ipanema, Botafogo, Leme e Humaitá."
         
     }
 }
