9186f3b2f07e67d2d05c3bbd3141c684bf7a8167
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-09-30 12:55:31 -0300

    fix the done button to receive programmatically the target, also the border radius color when firstResponder

--- ViajAI/Modules/Search/SearchViewController.swift
+++ ViajAI/Modules/Search/SearchViewController.swift
@@ -8,22 +8,119 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet weak var cityView: UIView!
+    @IBOutlet var cityTextField: UITextField!
+    @IBOutlet weak var countryView: UIView!
+    @IBOutlet var countryTextField: UITextField!
+    @IBOutlet weak var arriveDateView: UIView!
+    @IBOutlet var arriveDateTextField: UITextField!
+    @IBOutlet weak var exitDateView: UIView!
+    @IBOutlet var exitDateTextField: UITextField!
+    @IBOutlet weak var budgetView: UIView!
+    @IBOutlet var budgetTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configElements()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configElements() {
+        countryTextField.delegate = self
+        cityTextField.delegate = self
+        arriveDateTextField.delegate = self
+        exitDateTextField.delegate = self
+        budgetTextField.delegate = self
+        
+        countryTextField.textContentType = .countryName
+        cityTextField.textContentType = .addressCity
+        arriveDateTextField.textContentType = .dateTime
+        exitDateTextField.textContentType = .dateTime
+        
+        budgetTextField.keyboardType = .numberPad
+        arriveDateTextField.keyboardType = .numberPad
+        exitDateTextField.keyboardType = .numberPad
+        
+        budgetTextField.addDoneButtonToKeyboard(myAction: #selector(self.budgetTextField.resignFirstResponder))
+        arriveDateTextField.addDoneButtonToKeyboard(myAction: #selector(arriveDateDoneAction), target: self)
+        exitDateTextField.addDoneButtonToKeyboard(myAction: #selector(exitDateDoneAction), target: self)
     }
-    */
+    
+    @objc func arriveDateDoneAction() {
+        self.exitDateTextField.becomeFirstResponder()
+    }
+    
+    @objc func exitDateDoneAction() {
+        self.budgetTextField.becomeFirstResponder()
+    }
 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        var keyboardFrame: CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = view.convert(keyboardFrame, from: nil)
+
+        var contentInset: UIEdgeInsets = scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height + 20
+        scrollView.contentInset = contentInset
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        let contentInset: UIEdgeInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
+}
+
+
+extension SearchViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        switch textField {
+        case cityTextField:
+            self.cityView.layer.borderColor = UIColor.blue.cgColor
+        case countryTextField:
+            self.countryView.layer.borderColor = UIColor.blue.cgColor
+        case arriveDateTextField:
+            self.arriveDateView.layer.borderColor = UIColor.blue.cgColor
+        case exitDateTextField:
+            self.exitDateView.layer.borderColor = UIColor.blue.cgColor
+        case budgetTextField:
+            self.budgetView.layer.borderColor = UIColor.blue.cgColor
+        default:
+            textField.resignFirstResponder()
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch textField {
+        case cityTextField:
+            self.cityView.layer.borderColor = UIColor(red: 0.133, green: 0.133, blue: 0.133, alpha: 1).cgColor
+        case countryTextField:
+            self.countryView.layer.borderColor = UIColor(red: 0.133, green: 0.133, blue: 0.133, alpha: 1).cgColor
+        case arriveDateTextField:
+            self.arriveDateView.layer.borderColor = UIColor(red: 0.133, green: 0.133, blue: 0.133, alpha: 1).cgColor
+        case exitDateTextField:
+            self.exitDateView.layer.borderColor = UIColor(red: 0.133, green: 0.133, blue: 0.133, alpha: 1).cgColor
+        case budgetTextField:
+            self.budgetView.layer.borderColor = UIColor(red: 0.133, green: 0.133, blue: 0.133, alpha: 1).cgColor
+        default:
+            textField.resignFirstResponder()
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case cityTextField:
+            self.countryTextField.becomeFirstResponder()
+        case countryTextField:
+            self.arriveDateTextField.becomeFirstResponder()
+//        case budgetTextField:
+            // Open the category selection
+        default:
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    
 }
--- ViajAI/Utilities/AddDoneButtonToTextField.swift
+++ ViajAI/Utilities/AddDoneButtonToTextField.swift
@@ -9,12 +9,12 @@ import Foundation
 import UIKit
 
 extension UITextField{
- func addDoneButtonToKeyboard(myAction:Selector?){
+    func addDoneButtonToKeyboard(myAction:Selector?, target: Any? = UITextField.self){
     let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 300, height: 40))
     doneToolbar.barStyle = UIBarStyle.default
 
      let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
-     let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: myAction)
+     lazy var done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: target, action: myAction)
 
     var items = [UIBarButtonItem]()
     items.append(flexSpace)
