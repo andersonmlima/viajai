2a27b223551337832c4b2461104e5a9eeec6adf9
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-09-30 16:48:33 -0300

    fix the self passed to target

--- ViajAI/Utilities/AddDoneButtonToTextField.swift
+++ ViajAI/Utilities/AddDoneButtonToTextField.swift
@@ -9,12 +9,12 @@ import Foundation
 import UIKit
 
 extension UITextField{
-    func addDoneButtonToKeyboard(myAction:Selector?, target: Any? = UITextField.self){
+    func addDoneButtonToKeyboard(myAction:Selector?, target: Any? = nil){
     let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 300, height: 40))
     doneToolbar.barStyle = UIBarStyle.default
 
      let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
-     lazy var done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: target, action: myAction)
+     lazy var done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: target ?? self, action: myAction)
 
     var items = [UIBarButtonItem]()
     items.append(flexSpace)
