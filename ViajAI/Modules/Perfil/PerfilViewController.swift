1a49908818d59f45c7d7716f1ddf210f6bd71ec6
Fábio Oliveira Alegre <fabioalegre12@gmail.com>
2023-10-04 22:06:45 -0300

    Feita toda storyboard, e adicionado todas ligacoes e imagens da tela.

--- ViajAI/Modules/Perfil/PerfilViewController.swift
+++ ViajAI/Modules/Perfil/PerfilViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class PerfilViewController: UIViewController {
-
+    
+    // Outlets para os elementos da interface do usuário
+    @IBOutlet weak var perfilImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var changePhotoPerfilButton: UIButton!
+    @IBOutlet weak var verificationButton: UIButton!
+    @IBOutlet weak var perfilButton: UIButton!
+    @IBOutlet weak var preferencesButton: UIButton!
+    @IBOutlet weak var leaveButton: UIButton!
+    @IBOutlet weak var perfilDescriptionLabel: UILabel!
+    @IBOutlet weak var preferenceDescriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        changePhotoPerfilButton.layer.borderWidth = 1.0
+        changePhotoPerfilButton.layer.borderColor = UIColor.lightGray.cgColor
+        changePhotoPerfilButton.layer.cornerRadius = 20
+        
+        verificationButton.layer.borderWidth = 1.0
+        verificationButton.layer.borderColor = UIColor.lightGray.cgColor
+        verificationButton.layer.cornerRadius = 20
+    }
+    @IBAction func goLoginButton(_ sender: UIButton) {
+//        let viewController = UIStoryboard(name: "LoginViewController", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController
+//
+//            present(viewController ?? UIViewController(), animated: true)
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
-}
